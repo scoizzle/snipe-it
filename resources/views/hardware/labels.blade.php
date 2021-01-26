ef3660ee1228e258006142579f13dc9e54de846b
Scot Murphy <scot@scoizzle.com>
2021-01-25 21:51:03 -0600

    Created sane CSS for Label Printing.

--- resources/views/hardware/labels.blade.php
+++ resources/views/hardware/labels.blade.php
@@ -7,94 +7,87 @@
 </head>
 <body>
 
-<?php
-$settings->labels_width = $settings->labels_width - $settings->labels_display_sgutter;
-$settings->labels_height = $settings->labels_height - $settings->labels_display_bgutter;
-// Leave space on bottom for 1D barcode if necessary
-$qr_size = ($settings->alt_barcode_enabled=='1') && ($settings->alt_barcode!='') ? $settings->labels_height - .3 : $settings->labels_height - .3;
-?>
-
 <style>
     body {
+        display: grid;
+
+        column-gap: {{ $settings->labels_display_sgutter }}in;
+        grid-template-rows: repeat({{ floor($settings->labels_pagewidth / $settings->labels_width) }}, {{ $settings->labels_width }}in);
+
+        font-size: {{ $settings->labels_fontsize }}pt;
         font-family: arial, helvetica, sans-serif;
+
         width: {{ $settings->labels_pagewidth }}in;
         height: {{ $settings->labels_pageheight }}in;
-        margin: {{ $settings->labels_pmargin_top }}in {{ $settings->labels_pmargin_right }}in {{ $settings->labels_pmargin_bottom }}in {{ $settings->labels_pmargin_left }}in;
-        font-size: {{ $settings->labels_fontsize }}pt;
+
+        margin: 0px;
+        padding-top: {{ $settings->labels_pmargin_top }}in;
+        padding-right: {{ $settings->labels_pmargin_right }}in;
+        padding-bottom: {{ $settings->labels_pmargin_bottom }}in;
+        padding-left: {{ $settings->labels_pmargin_left }}in;
     }
+
     .label {
+        display: grid;
+        
+        column-gap: .15in;
+        grid-template-areas:
+            "qr_text    qr_text     qr_text"
+            "qr_img     company     company"
+            "qr_img     name        name"
+            "qr_img     tag         tag"
+            "qr_img     model       model"
+            "qr_img     serial      serial"
+            "barcode    barcode     barcode";
+
         width: {{ $settings->labels_width }}in;
         height: {{ $settings->labels_height }}in;
-        padding: 0in;
-        margin-right: {{ $settings->labels_display_sgutter }}in; /* the gutter */
-        margin-bottom: {{ $settings->labels_display_bgutter }}in;
+
+        padding: 0.05in 0.25in;
         display: inline-block;
         overflow: hidden;
     }
+
     .page-break  {
-        page-break-after:always;
+        page-break-after: always;
     }
-    div.qr_img {
-        width: {{ $qr_size }}in;
-        height: {{ $qr_size }}in;
 
-        float: left;
-        display: inline-flex;
-        padding-right: .15in;
-    }
-    img.qr_img {
+    div.company { grid-area: company; }
+    div.name { grid-area: name; }
+    div.tag { grid-area: tag; }
+    div.model { grid-area: model; }
+    div.serial { grid-area: serial; }
 
-        width: 120.79%;
-        height: 120.79%;
-        margin-top: -6.9%;
-        margin-left: -6.9%;
-        padding-bottom: .04in;
-    }
-    img.barcode {
-        display:block;
+    img.qr_img { grid-area: qr_img; }
 
-        padding-top: .11in;
-        width: 100%;
-    }
     .qr_text {
-        width: {{ $settings->labels_width }}in;
-        height: {{ $settings->labels_height }}in;
-        padding-top: {{$settings->labels_display_bgutter}}in;
+        grid-area: qr_text;
+
         font-family: arial, helvetica, sans-serif;
         font-size: {{$settings->labels_fontsize}};
-        padding-right: .01in;
         overflow: hidden !important;
         display: inline;
         word-wrap: break-word;
         word-break: break-all;
     }
-    div.barcode_container {
+
+    img.barcode {
+        grid-area: barcode
 
         width: 100%;
         display: inline;
         overflow: hidden;
+
+        padding-top: .11in;
     }
-    .next-padding {
-        margin: {{ $settings->labels_pmargin_top }}in {{ $settings->labels_pmargin_right }}in {{ $settings->labels_pmargin_bottom }}in {{ $settings->labels_pmargin_left }}in;
-    }
-    @media print {
-        .noprint {
-            display: none !important;
-        }
-        .next-padding {
-            margin: {{ $settings->labels_pmargin_top }}in {{ $settings->labels_pmargin_right }}in {{ $settings->labels_pmargin_bottom }}in {{ $settings->labels_pmargin_left }}in;
-            font-size: 0;
-        }
-    }
+
+
     @media screen {
         .label {
-            outline: .02in black solid; /* outline doesn't occupy space like border does */
-        }
-        .noprint {
-            font-size: 13px;
-            padding-bottom: 15px;
+            outline: .02in black solid;
         }
     }
+
     @if ($snipeSettings->custom_css)
         {{ $snipeSettings->show_custom_css() }}
     @endif
@@ -105,57 +98,50 @@ $qr_size = ($settings->alt_barcode_enabled=='1') && ($settings->alt_barcode!='')
     <div class="label">
 
         @if ($settings->qr_code=='1')
-            <div class="qr_img">
-                <img src="./{{ $asset->id }}/qr_code" class="qr_img">
+            <img src="./{{ $asset->id }}"/qr_code" class="qr_img">
+        @endif
+
+        
+        @if ($settings->qr_text!='')
+            <strong class="qr_text">>{{ $settings->qr_text }}</strong>
+        @endif
+
+        @if (($settings->labels_display_company_name=='1') && ($asset->company))
+            <div class="pull-left company">
+                C: {{ $asset->company->name }}
             </div>
         @endif
 
-        <div class="qr_text">
-            @if ($settings->qr_text!='')
-                <div class="pull-left">
-                    <strong>{{ $settings->qr_text }}</strong>
-                    <br>
-                </div>
-            @endif
-            @if (($settings->labels_display_company_name=='1') && ($asset->company))
-                <div class="pull-left">
-                    C: {{ $asset->company->name }}
-                </div>
-            @endif
-            @if (($settings->labels_display_name=='1') && ($asset->name!=''))
-                <div class="pull-left">
-                    N: {{ $asset->name }}
-                </div>
-            @endif
-            @if (($settings->labels_display_tag=='1') && ($asset->asset_tag!=''))
-                <div class="pull-left">
-                    T: {{ $asset->asset_tag }}
-                </div>
-            @endif
-            @if (($settings->labels_display_serial=='1') && ($asset->serial!=''))
-                <div class="pull-left">
-                    S: {{ $asset->serial }}
-                </div>
-            @endif
-            @if (($settings->labels_display_model=='1') && ($asset->model->name!=''))
-                <div class="pull-left">
-                    M: {{ $asset->model->name }} {{ $asset->model->model_number }}
-                </div>
-            @endif
+        @if (($settings->labels_display_name=='1') && ($asset->name!=''))
+            <div class="pull-left name">
+                N: {{ $asset->name }}
+            </div>
+        @endif
 
-        </div>
+        @if (($settings->labels_display_tag=='1') && ($asset->asset_tag!=''))
+            <div class="pull-left tag">
+                T: {{ $asset->asset_tag }}
+            </div>
+        @endif
+
+        @if (($settings->labels_display_serial=='1') && ($asset->serial!=''))
+            <div class="pull-left serial">
+                S: {{ $asset->serial }}
+            </div>
+        @endif
+
+        @if (($settings->labels_display_model=='1') && ($asset->model->name!=''))
+            <div class="pull-left model">
+                M: {{ $asset->model->name }} {{ $asset->model->model_number }}
+            </div>
+        @endif
 
         @if ((($settings->alt_barcode_enabled=='1') && $settings->alt_barcode!=''))
-            <div class="barcode_container">
-                <img src="./{{ $asset->id }}/barcode" class="barcode">
-            </div>
+            <img src="./{{ $asset->id }}/barcode" class="barcode">
         @endif
-
-
-
     </div>
 
-    @if ($count % $settings->labels_per_page == 0)
+    @if ($count % $settings->labels_per_page != 0)
         <div class="page-break"></div>
         <div class="next-padding">&nbsp;</div>
     @endif
